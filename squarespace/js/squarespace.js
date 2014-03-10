44e7e3b5c28b6e61a8912334a92489c7a99eb558
Ryan Lee <rlee928@gmail.com>
2014-03-09 23:43:18 -0400

    changes

---
+++ squarespace/js/squarespace.js
@@ -0,0 +1,132 @@
+$(document).ready(function(){
+    // show popup when you click
+    $('.one').click(function(event){
+        event.preventDefault(); // disable normal link function so that it doesn't refresh the page
+        $('.one-bg').show(); //display popup
+    });
+
+    // hide popup when user clicks on close button
+    $('.close-btn').click(function(){
+    	$('.one-bg').hide(); // hide the overlay
+    });
+
+    // hides the popup if user clicks anywhere outside the container
+    $('.one-bg').click(function(){
+    	$('.one-bg').hide();
+    })
+    // prevents the overlay from closing if user clicks inside the popup overlay
+    $('.one-content').click(function(){
+		return false;
+	});
+
+
+
+    // show popup when you click
+    $('.two').click(function(event){
+        event.preventDefault(); // disable normal link function so that it doesn't refresh the page
+        $('.two-bg').show(); //display popup
+    });
+
+    // hide popup when user clicks on close button
+    $('.close-btn').click(function(){
+        $('.two-bg').hide(); // hide the overlay
+    });
+
+    // hides the popup if user clicks anywhere outside the container
+    $('.two-bg').click(function(){
+        $('.two-bg').hide();
+    })
+    // prevents the overlay from closing if user clicks inside the popup overlay
+    $('.two-content').click(function(){
+        return false;
+    });
+
+
+
+        // show popup when you click
+    $('.three').click(function(event){
+        event.preventDefault(); // disable normal link function so that it doesn't refresh the page
+        $('.three-bg').show(); //display popup
+    });
+
+    // hide popup when user clicks on close button
+    $('.close-btn').click(function(){
+        $('.three-bg').hide(); // hide the overlay
+    });
+
+    // hides the popup if user clicks anywhere outside the container
+    $('.three-bg').click(function(){
+        $('.three-bg').hide();
+    })
+    // prevents the overlay from closing if user clicks inside the popup overlay
+    $('.three-content').click(function(){
+        return false;
+    });	
+
+
+
+        // show popup when you click
+    $('.four').click(function(event){
+        event.preventDefault(); // disable normal link function so that it doesn't refresh the page
+        $('.four-bg').show(); //display popup
+    });
+
+    // hide popup when user clicks on close button
+    $('.close-btn').click(function(){
+        $('.four-bg').hide(); // hide the overlay
+    });
+
+    // hides the popup if user clicks anywhere outside the container
+    $('.four-bg').click(function(){
+        $('.four-bg').hide();
+    })
+    // prevents the overlay from closing if user clicks inside the popup overlay
+    $('.four-content').click(function(){
+        return false;
+    });
+
+
+
+        // show popup when you click
+    $('.five').click(function(event){
+        event.preventDefault(); // disable normal link function so that it doesn't refresh the page
+        $('.five-bg').show(); //display popup
+    });
+
+    // hide popup when user clicks on close button
+    $('.close-btn').click(function(){
+        $('.five-bg').hide(); // hide the overlay
+    });
+
+    // hides the popup if user clicks anywhere outside the container
+    $('.five-bg').click(function(){
+        $('.five-bg').hide();
+    })
+    // prevents the overlay from closing if user clicks inside the popup overlay
+    $('.five-content').click(function(){
+        return false;
+    });
+
+
+
+    // show popup when you click
+    $('.six').click(function(event){
+        event.preventDefault(); // disable normal link function so that it doesn't refresh the page
+        $('.six-bg').show(); //display popup
+    });
+
+    // hide popup when user clicks on close button
+    $('.close-btn').click(function(){
+        $('.six-bg').hide(); // hide the overlay
+    });
+
+    // hides the popup if user clicks anywhere outside the container
+    $('.six-bg').click(function(){
+        $('.six-bg').hide();
+    })
+    // prevents the overlay from closing if user clicks inside the popup overlay
+    $('.six-content').click(function(){
+        return false;
+    });
+
+});
